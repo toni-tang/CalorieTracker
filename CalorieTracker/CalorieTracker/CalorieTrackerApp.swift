2cb7af9c3d528d80f9025975f3cae82060514e9c
Tony Tang <tonytang213@gmail.com>
2022-08-05 23:17:12 -0500

    Initial commit

---
+++ CalorieTracker/CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Tony Tang on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
